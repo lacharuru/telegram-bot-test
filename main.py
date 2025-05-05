349305915718aa21fd5bee6f3ea2e8a4d2994cda
Ana <150439848+lacharuru@users.noreply.github.com>
2025-05-05 09:50:42 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+# bot token (be sure to copy-paste it correctly)
+TELEGRAM_BOT_TOKEN = "xxxxxx"
+# Telegram Chat ID, you can use a group ID 
+CHAT_ID = "xxxxxx"
+
+MESSAGE = "Yay, i'm here!"
+
+# Send message trough API
+response = requests.post(
+    f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
+    json={
+        "chat_id": CHAT_ID,
+        "text": MESSAGE,
+        "parse_mode": "HTML",  # Can use HTML or markdown
+        "disable_web_page_preview": True
+    }
+)
+
+# Message status verifier
+if response.ok:
+    print("[✓] Oki doki!")
+else:
+    print(f"[✗] Something went wrong: {response.status_code} - {response.text}")
